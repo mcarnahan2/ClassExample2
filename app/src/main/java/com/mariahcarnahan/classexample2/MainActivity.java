9dffdd9fb405494e28eb8beb97e39a10c5ad8fba
mcarnahan2 <46825203+mcarnahan2@users.noreply.github.com>
2019-01-23 12:57:45 -0600

    Change text to Italian

--- app/src/main/java/com/mariahcarnahan/classexample2/MainActivity.java
+++ app/src/main/java/com/mariahcarnahan/classexample2/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ((TextView) findViewById(R.id.textView)).setText("Gueten tag, weld!!");
+                ((TextView) findViewById(R.id.textView)).setText("Ciao, bello!!");
             }
         });
     }
